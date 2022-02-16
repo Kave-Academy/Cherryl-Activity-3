fa937488ee9673afdc6cda6d3036ecba951ca8e7
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-16 11:06:30 +0800

    Display the input text value with the word Not Prime/Prime

--- exer.js
+++ exer.js
@@ -1,2 +1,14 @@
-function run() {
+function run(num) {
+    let param = document.getElementById("input_number").value;
+
+
+    if (param % 2 === 0){
+
+        document.getElementById("output").innerHTML = param + "  Not Prime Number ";
+       
+    } else {
+        document.getElementById("output").innerHTML = param + " Prime Number ";
+    }
 }
+    
+    
