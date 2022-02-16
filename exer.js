b5dcbf81ab76710e72abfd06e1bd5d8cab92431e
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-16 14:02:17 +0800

    Finished the activity 3 that accepts a number and check if prime or not

--- exer.js
+++ exer.js
@@ -1,14 +1,23 @@
-function run(num) {
-    let param = document.getElementById("input_number").value;
+//Function to check number
+function run() {
 
+    var param, i, bool = true;
 
-    if (param % 2 === 0){
+    //get the value from input text - DOM
+    param = document.getElementById("input_number").value;
 
-        document.getElementById("output").innerHTML = param + "  Not Prime Number ";
-       
-    } else {
-        document.getElementById("output").innerHTML = param + " Prime Number ";
-    }
+    for (i=2; i <= param -1 ; i++)
+            if(param % i == 0){
+                bool = false;
+                break;
+                
+            }
+
+            //Check and display in UI
+            if (bool == true)
+            document.getElementById("output").innerHTML = param + " Prime Number ";
+            else 
+            document.getElementById("output").innerHTML = param + " Not Prime Number ";
 }
-    
+// } https://www.geeksforgeeks.org/check-a-number-is-prime-or-not-using-javascript/#:~:text=A%20prime%20number%20is%20a,alert%20message%20on%20the%20screen.
     
